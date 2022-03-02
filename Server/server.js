556a942a9e9584d391a7ac0def0f0fb3ad6e64d5
mwezn <m.w.g.nelson@gmail.com>
2022-03-02 18:37:29 +0000

    I added ejs templates

--- Server/server.js
+++ Server/server.js
@@ -17,7 +17,7 @@ const webData = require("../data")
 
 app.post('/search', (req, res)=>{
   console.log(req,req.body.inputTxt)
-  res.send(`your results for ${req.body.inputTxt}`)
+  res.render('results.ejs', {data: req.body.inputTxt})
 })
 
 app.get('/', (req, res)=>{
