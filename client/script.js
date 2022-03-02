9a0ea370e91f16cbefc417c6cbfdb8f03cd14133
jesse dodoo <jesse.dodoo@gmail.com>
2022-03-02 14:35:57 +0000

    changed function to an asyn function

--- client/script.js
+++ client/script.js
@@ -1,11 +1,29 @@
 
-function getRandomQuote(e) {
+async function getSite(e) {
     e.preventDefault()
-    
 
-    fetch('http://localhost:3000/search')
-      .then(resp => resp.json())
-      .then(d=>console.log(d))
+    const res = await fetch('http://localhost:3000/search')
+      const data = await res.json()
+      console.log(data)
   }
   const form = document.querySelector('form')
-  form.addEventListener('submit', getRandomQuote)
+  form.addEventListener('submit', getSite)
+
+
+//   let form = document.querySelector('form')
+// let quoteID = document.getElementById('quote')
+// let authorID = document.getElementById('author')
+
+
+// async function getQuote(e) {
+//   e.preventDefault(e)
+//   const res = await fetch("http://localhost:3000/quotes/quote/random");
+//   const data = await res.json();
+
+//   const quote = data.quote
+//   const author = data.author
+//   quoteID.innerHTML = (capitalize(quote))
+//   authorID.innerHTML = (authorName(author))
+//   console.log(quote)
+//   console.log(author)
+// }
