d3c23118849acaf409919eea3f67acdb50e4a0a8
jesse dodoo <jesse.dodoo@gmail.com>
2022-03-02 14:01:59 +0000

    created data.js and populated with 10 webpages

---
+++ data.js
@@ -0,0 +1,15 @@
+const weblinks = [
+    {id: 1, web: '"https://www.facebook.com/"'},
+    {id: 2, web: '"https://www.youtube.com/"'},
+    {id: 3, web: '"https://www.amazon.co.uk/"'},
+    {id: 4, web: '"https://weather.com/en-GB/"'},
+    {id: 5, web: '"https://www.arsenal.com/"'},
+    {id: 6, web: '"https://www.skysports.com/"'},
+    {id: 7, web: '"https://www.asos.com/"'},
+    {id: 8, web: '"https://www.instagram.com/?hl=en"'},
+    {id: 9, web: '"https://twitter.com/home?lang=en"'},
+    {id: 10, web: '"https://github.com/"'},
+   
+]
+
+module.exports = weblinks
